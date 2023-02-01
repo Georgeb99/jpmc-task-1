894f15e843eb4b97596ce7731615631f65e222f1
Georgeb99 <124191456+Georgeb99@users.noreply.github.com>
2023-02-01 18:04:51 +0000

    Update client.py

--- client.py
+++ client.py
@@ -35,18 +35,23 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    #if price_b == 0, then price_a/price_b would be problematic - nothing divided by 0 goes well! A quick filter to divvy up values of price_b between 0 and non-zero values is all that is necessary to prevent any issues.
+    if (price_b == 0):
+        return
+    return (price_a/price_b)
 
 
 # Main
 if __name__ == "__main__":
+    # Below is a relatively sloppy solution - I am still learning a lot when it comes to python, and a constantly filling/emptying list seemed like the most apt solution
+    TempStorage=[]
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
@@ -55,5 +60,8 @@ if __name__ == "__main__":
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-
-        print("Ratio %s" % getRatio(price, price))
+            #Fill TempStorage so that I can call getRatio with the correct values
+            TempStorage.append(price)
+        print("Ratio %s" % getRatio(TempStorage[0], TempStorage[1]))
+        #It is essential to clear the list otherwise the same value will be repeated ad infinitum, which is unideal as that does not occur in reality.
+        TempStorage.clear()
